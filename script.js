9dd073310ab17e9a33f5bd20eab4064ebbecedf4
inioluwa <ajayiinioluwa2007@gmail.com>
2025-10-15 06:12:56 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const multiply = () => {
+    console.log(firstNumber.value);
+    console.log(secondNumber.value);
+    const answer = firstNumber.value * secondNumber.value
+    console.log(answer);
+    showResult.innerHTML = answer
+    
+}
+
+const divide = () => {
+    console.log(firstNumber.value);
+    console.log(secondNumber.value);
+    const answer = firstNumber.value / secondNumber.value
+    console.log(answer);
+    showResult.innerHTML = answer
+    
+}
+
+const add = () => {
+    console.log(Number(firstNumber.value));
+    console.log(Number(secondNumber.value));
+    const answer = Number(firstNumber.value) + Number(secondNumber.value);
+    console.log(Number(answer));
+    showResult.innerHTML = answer
+    
+}
+
+const subtract = () => {
+    console.log(firstNumber.value);
+    console.log(secondNumber.value);
+    const answer = Number(firstNumber.value) - Number(secondNumber.value)
+    console.log(Number(answer));
+    showResult.innerHTML = answer
+    
+}
